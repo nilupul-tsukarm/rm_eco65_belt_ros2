f72b7d318c00bec7af2cc8d13471191b2fbc9da0
nilupul-tsukarm <nilupulsenavirathna@TsukArm.co.jp>
2024-12-23 17:25:07 +0900

    gripper,rotate activated

--- old/smart_scape_main7.py
+++ old/smart_scape_main7.py
@@ -4,14 +4,12 @@ from rclpy.node import Node
 from geometry_msgs.msg import Pose, PointStamped, TransformStamped
 from rm_ros_interfaces.msg import Movel
 from std_msgs.msg import Bool, Float32, String
-from sensor_msgs.msg import JointState
 from rclpy.executors import MultiThreadedExecutor
 from std_srvs.srv import SetBool, Empty
 import json
 import tf2_ros
 import time
 import struct
-import serial
 
 # Helper functions (Your existing functions)
 def calculate_crc(data):
@@ -105,7 +103,7 @@ class RobotArmController(Node):
             pose.orientation.y = 0.6987419724464417
             pose.orientation.z = 0.015018999576568604
             pose.orientation.w = -0.002392000053077936
-        self.send_linear_motion(pose, speed=100, block=True)
+        self.send_linear_motion(pose, speed=90, block=True)
         
     def go_up_from_mid_belt_position(self):
         pose = Pose()
@@ -116,7 +114,7 @@ class RobotArmController(Node):
         pose.orientation.y = -0.010149000212550163
         pose.orientation.z = 0.012520000338554382
         pose.orientation.w = 0.00880299974232912
-        self.send_linear_motion(pose, speed=100, block=True)
+        self.send_linear_motion(pose, speed=90, block=True)
 
     def go_to_cup_A_position(self):
         pose = Pose()
@@ -127,7 +125,7 @@ class RobotArmController(Node):
         pose.orientation.y = -0.010149000212550163
         pose.orientation.z = 0.012520000338554382
         pose.orientation.w = 0.00880299974232912
-        self.send_linear_motion(pose, speed=100, block=True)
+        self.send_linear_motion(pose, speed=90, block=True)
 
     def go_to_cup_B_position(self):
         pose = Pose()
@@ -138,7 +136,7 @@ class RobotArmController(Node):
         pose.orientation.y = -0.010149000212550163
         pose.orientation.z = 0.012520000338554382
         pose.orientation.w = 0.00880299974232912
-        self.send_linear_motion(pose, speed=100, block=True)
+        self.send_linear_motion(pose, speed=90, block=True)
 
 
 class TCPPacketSubscriber(Node):
@@ -182,8 +180,7 @@ class TCPPacketSubscriber(Node):
                         "block_id": id_,
                         "x_offset": center["x"],
                         "y_offset": center["y"],
-                        "x_size": size["x"],
-                        "y_size": size["y"],
+                        "size": size,
                         "label": label
                     })
                     self.processed_ids.add(id_)
@@ -197,7 +194,7 @@ class TCPPacketSubscriber(Node):
 
 
 class LegoTracker(Node):
-    def __init__(self, arm_controller,gripper_controller):
+    def __init__(self, arm_controller):
         super().__init__('lego_tracker')
         self.belt_speed_sub = self.create_subscription(Float32, '/belt_speed', self.belt_speed_callback, 10)
         self.block_data_sub = self.create_subscription(String, '/lego_blocks', self.block_data_callback, 10)
@@ -206,9 +203,6 @@ class LegoTracker(Node):
         self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
         
         self.arm_controller = arm_controller  # Reference to the arm controller
-        self.arm_controller.go_up_from_mid_belt_position()
-        
-        self.gripper_controller = gripper_controller
 
         # Configurable parameters
         self.belt_speed = 0.0
@@ -223,8 +217,8 @@ class LegoTracker(Node):
 
 
     def belt_speed_callback(self, msg):
-        self.belt_speed = abs(msg.data)
-        # self.belt_speed = 10.0 
+        self.belt_speed = abs(msg.data) # get absolute value
+        #self.belt_speed = 10.0  #if neeed pass fixed value
         
         
     def block_data_callback(self, msg):
@@ -243,19 +237,15 @@ class LegoTracker(Node):
             block_id = block["block_id"]
             x_offset = block["x_offset"]
             y_offset = block["y_offset"]
-            x_size = block["x_size"]
-            y_size = block["y_size"]
-            self.get_logger().info(f" x_size: {x_size}")
-            self.get_logger().info(f" y_size: {y_size}")
+            #self.get_logger().info(f" blocks: {block}")
             if block_id not in self.blocks:
-                self.blocks[block_id] = {"x": x_offset, "y": y_offset, "y_size": y_size, "time": current_time}
+                self.blocks[block_id] = {"x": x_offset, "y": y_offset, "time": current_time}
 
     def update_blocks(self):
         current_time = self.get_clock().now()
         for block_id in list(self.blocks.keys()):
             start_x = self.blocks[block_id]["x"]
             start_y = self.blocks[block_id]["y"]
-            y_size = self.blocks[block_id]["y_size"]
             time_elapsed = current_time - self.blocks[block_id]["time"]
             # Convert time_elapsed from Duration to seconds (float)
             time_elapsed_seconds = time_elapsed.nanoseconds / 1e9
@@ -267,18 +257,12 @@ class LegoTracker(Node):
                 continue
             
             if distance_moved > self.pickup_boundary :
-                if y_size > 0.2 : # large
-                    self.arm_controller.go_to_mid_belt_position(0,0)
-                else:
-                    self.arm_controller.go_to_mid_belt_position(0,90)
-                time.sleep(1)
-                self.gripper_controller.set_position(1) #gripper close
-                time.sleep(0.3)
+                self.arm_controller.go_up_from_mid_belt_position()
+                self.arm_controller.go_to_mid_belt_position(0,90)
                 self.arm_controller.go_to_cup_A_position()
-                time.sleep(1)
-                self.gripper_controller.set_position(0) #gripper open
                 self.arm_controller.go_up_from_mid_belt_position()
                 del self.blocks[block_id]
+                time.sleep(1)
             
             start_point_x = - 0.5
             
@@ -310,97 +294,22 @@ class LegoTracker(Node):
         self.track_blocks(mock_tcp_data)
         self.update_blocks()'''
 
-class GripperControlNode(Node):
-    def __init__(self):
-        super().__init__('gripper_control')
-        self.declare_parameter('serial_port', '/dev/ttyUSB0')
-        self.declare_parameter('baud_rate', 115200)
-
-        # Initialize Serial Communication
-        serial_port = self.get_parameter('serial_port').get_parameter_value().string_value
-        baud_rate = self.get_parameter('baud_rate').get_parameter_value().integer_value
-
-        try:
-            self.ser = serial.Serial(port=serial_port, baudrate=baud_rate, timeout=2)
-            if self.ser.isOpen():
-                self.get_logger().info("Gripper serial connection established.")
-            else:
-                raise serial.SerialException("Failed to open serial port.")
-        except serial.SerialException as e:
-            self.get_logger().error(f"Serial connection error: {e}")
-            exit()
-
-        # ROS Services and Publishers
-        self.publisher = self.create_publisher(JointState, 'gripper_joint_states', 10)
-        #self.timer = self.create_timer(0.1, self.publish_joint_states)
-        self.timer = self.create_timer(0.1, self.publish_joint_states)  # 50 Hz (every 20 ms)
-
-        
-        # Initialize Gripper
-        self.gripper_position = 0  # Start with a default position (e.g., 0)
-
-        # Initialize Gripper
-        self.initialize_gripper()
-        self.initialize_speed()
-        
-
-    def initialize_gripper(self):
-        init_command = b'\x01\x06\x01\x00\x00\x01\x49\xF6'
-        self.ser.write(init_command)
-        response = self.ser.read(8)
-        self.get_logger().info(f"Gripper Initialization Response: {response}")
-        
-    def initialize_speed(self):
-        # Speed command (example, may need adjustment depending on your needs)
-        speed_command = build_command(0x01, 0x06, 0x0104, 0x0032)
-        max_speed_command = build_command(0x01, 0x06, 0x0104, 0x0064)
-        speed_90_command = build_command(0x01, 0x06, 0x0104, 0x005A)
-        self.ser.write(max_speed_command)
-        response = self.ser.read(8)
-        print("Speed Response:", response)
-
-    def set_position(self, state):
-        if state :  # 1 = true open
-            decimal_value = 100
-        else:
-            decimal_value = 1000
-        hex_value = decimal_to_hex(decimal_value)
-        self.gripper_position = decimal_value
-        position_command = build_command(0x01, 0x06, 0x0103, hex_value)
-        self.ser.write(position_command)
-        response = self.ser.read(8)
-        self.get_logger().info(f"Gripper Position {decimal_value} Response: {response}")
-
-    def publish_joint_states(self):
-        joint_state = JointState()
-        joint_state.header.stamp = self.get_clock().now().to_msg()
-        joint_state.name = ['gripper_joint']
-        joint_state.position = [self.gripper_position / 1.0]  # Normalize or adjust if necessary
-        self.publisher.publish(joint_state)
-
-    def destroy_node(self):
-        self.ser.close()
-        self.get_logger().info("Serial connection closed.")
-        super().destroy_node()
 
 def main():
     rclpy.init()
 
     arm_controller = RobotArmController()
-    gripper_controller = GripperControlNode()
-    lego_tracker = LegoTracker(arm_controller,gripper_controller)
+    lego_tracker = LegoTracker(arm_controller)
     tcp_packet_subscriber = TCPPacketSubscriber()
 
     executor = MultiThreadedExecutor()
     executor.add_node(arm_controller)
-    executor.add_node(gripper_controller)
     executor.add_node(lego_tracker)
     executor.add_node(tcp_packet_subscriber)
 
     try:
         executor.spin()
     finally:
-        gripper_controller.destroy_node()
         arm_controller.destroy_node()
         lego_tracker.destroy_node()
         tcp_packet_subscriber.destroy_node()
